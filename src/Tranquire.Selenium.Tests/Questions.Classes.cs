0b7535c0287087ec9920ce86681ff81162cf5943
Lucas Loegel <lucas.loegel@outlook.com>
2016-03-29 01:25:59 +0200

    Add Classes questions

---
+++ src/Tranquire.Selenium.Tests/Questions.Classes.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tranquire.Selenium.Questions;
+using Xunit;
+
+namespace Tranquire.Selenium.Tests
+{
+    public partial class Questions
+    {
+        [Theory]
+        [InlineData("ClassesElement1", new[] { "highlighted" })]
+        [InlineData("ClassesElement2", new[] { "highlighted", "otherclass1", "otherclass2" })]
+        [InlineData("ClassesElement3", new[] { "highlighted", "otherclass1", "otherclass2" })]
+        [InlineData("ClassesElement4", new string[] { })]
+        [InlineData("ClassesElement5", new string[] { })]
+        public void Classes_ShouldReturnCorrectValue(string id, string[] expected)
+        {
+            //arrange
+            var target = Target.The("Element with classes").LocatedBy(By.Id(id));
+            var question = Classes.Of(target).Value;
+            //act            
+            var actual = Answer(question);
+            //assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ src/Tranquire.Selenium/Questions/Classes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace Tranquire.Selenium.Questions
+{
+    public class Classes : SingleUIState<ImmutableArray<string>, Classes>
+    {
+        public Classes(ITarget target):base(target)
+        {
+        }
+
+        public Classes(ITarget target, CultureInfo culture) : base(target, culture)
+        {
+        }
+
+        public static Classes Of(ITarget target)
+        {
+            return new Classes(target);
+        }
+
+        protected override Classes CreateState(ITarget target, CultureInfo culture)
+        {
+            return new Classes(target, culture);
+        }
+
+        protected override ImmutableArray<string> ResolveFor(IWebElement element)
+        {
+            return ImmutableArray.Create(element.GetAttribute("class").Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+        }
+    }
+}
