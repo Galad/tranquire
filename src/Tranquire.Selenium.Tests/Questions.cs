3c6b7c65d0d4446dfa7c8fec17faa3fa77825d17
Lucas Loegel <lucas.loegel@outlook.com>
2016-03-29 20:38:41 +0200

    Add page title question

---
+++ src/Tranquire.Selenium.Tests/Questions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tranquire.Selenium.Questions;
+using Xunit;
+
+namespace Tranquire.Selenium.Tests
+{
+    public partial class Questions
+    {
+        [Fact]
+        public void PageTitle_ShouldReturnCorrectValue()
+        {
+            //arrange
+            var question = Page.Title();
+            //act
+            var actual = Answer(question);
+            //assert
+            Assert.Equal("The title", actual);
+        }
+    }
+}
---
+++ src/Tranquire.Selenium/Questions/PageTitle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tranquire.Selenium.Questions
+{
+    public class PageTitle : IQuestion<string>
+    {
+        public string AnsweredBy(IActor actor)
+        {
+            return actor.BrowseTheWeb().Title;
+        }
+    }
+
+    public class Page
+    {
+        public static IQuestion<string> Title()
+        {
+            return new PageTitle();
+        }
+    }
+}
