05a6843838f3b5f4d91ad595f638df8a35dd4f04
Lucas Loegel <lucas.loegel@outlook.com>
2016-03-29 20:31:36 +0200

    Add Visibility question

---
+++ src/Tranquire.Selenium.Tests/Questions.Visible.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tranquire.Selenium.Questions;
+using Xunit;
+
+namespace Tranquire.Selenium.Tests
+{
+    public partial class Questions
+    {
+		[Theory]
+		[InlineData("VisibleElement", true)]
+		[InlineData("NotDisplayedElement", false)]
+        [InlineData("NotDisplayedElement2", false)]
+        public void VisibleElement_ShouldReturnCorrectValue(string id, bool expected)
+        {
+            //arrange
+            var target = Target.The("visible element").LocatedBy(By.Id(id));
+            var question = Visibility.Of(target).Value;
+            //act
+            var actual = Answer(question);
+            //assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ src/Tranquire.Selenium/Questions/Visibility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace Tranquire.Selenium.Questions
+{
+    public class Visibility : SingleUIState<bool, Visibility>
+    {
+        public Visibility(ITarget target, CultureInfo culture) : base(target, culture)
+        {
+        }
+
+        public Visibility(ITarget target) : base(target)
+        {
+        }
+
+        protected override Visibility CreateState(ITarget target, CultureInfo culture)
+        {
+            return new Visibility(target, culture);
+        }
+
+        public static Visibility Of(ITarget target)
+        {
+            return new Visibility(target);
+        }
+
+        protected override bool ResolveFor(IWebElement element)
+        {
+            return element.Displayed;
+        }
+    }
+}
