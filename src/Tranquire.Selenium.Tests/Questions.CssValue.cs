184d0d0ec8e102d489330aec6fa31594f00b27bf
Lucas Loegel <lucas.loegel@outlook.com>
2016-03-30 05:54:22 +0200

    Add CssValue question

---
+++ src/Tranquire.Selenium.Tests/Questions.CssValue.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tranquire.Selenium.Questions;
+using Xunit;
+
+namespace Tranquire.Selenium.Tests
+{
+    public partial class Questions
+    {
+        [Theory]
+        [InlineData("CssValueInlineStyle", "font-style", "italic")]
+        [InlineData("CssValueInlineStyle", "noproperty", "")]
+        [InlineData("CssValueClass", "font-style", "italic")]
+        [InlineData("CssValueClass", "noproperty", "")]
+        public void CssValueElement_ShouldReturnCorrectValue(string id, string cssProperty, string expected)
+        {
+            //arrange
+            var target = Target.The("css value element").LocatedBy(By.Id(id));
+            var question = CssValue.Of(target).AndTheProperty(cssProperty).Value;
+            //act
+            var actual = Answer(question);
+            //assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ src/Tranquire.Selenium/Questions/CssValue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace Tranquire.Selenium.Questions
+{
+    public class CssValue : SingleUIState<string, CssValue>
+    {
+        public string PropertyName { get; }
+
+        public CssValue(string propertyName, ITarget target) : base(target)
+        {
+            Guard.ForNullOrEmpty(propertyName, nameof(propertyName));
+            PropertyName = propertyName;
+        }
+
+        public CssValue(string propertyName, ITarget target, CultureInfo culture) : base(target, culture)
+        {
+            Guard.ForNullOrEmpty(propertyName, nameof(propertyName));
+        }
+
+        public static CssValueBuilder Of(ITarget target)
+        {
+            return new CssValueBuilder(target);
+        }
+
+        protected override CssValue CreateState(ITarget target, CultureInfo culture)
+        {
+            return new CssValue(PropertyName, target, culture);
+        }
+
+        protected override string ResolveFor(IWebElement element)
+        {
+            return element.GetCssValue(PropertyName);
+        }
+    }
+
+    public class CssValueBuilder
+    {
+        private readonly ITarget _target;
+
+        public CssValueBuilder(ITarget target)
+        {
+            _target = target;
+        }
+
+        public CssValue AndTheProperty(string propertyName)
+        {
+            return new CssValue(propertyName, _target);
+        }
+    }
+}
