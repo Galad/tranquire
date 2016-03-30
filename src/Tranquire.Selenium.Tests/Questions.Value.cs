63bd2d2999e031d4764b06663d52ef6160ef2639
Lucas Loegel <lucas.loegel@outlook.com>
2016-03-30 05:34:11 +0200

    Add Value question

---
+++ src/Tranquire.Selenium.Tests/Questions.Value.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tranquire.Selenium.Questions;
+using Xunit;
+
+namespace Tranquire.Selenium.Tests
+{
+    public partial class Questions
+    {
+        [Theory]
+		[InlineData("ValueElement", "the value")]
+        [InlineData("EmptyValueElement", "")]
+        [InlineData("NoValueElement", "")]
+        [InlineData("OptionValueElement", "1")]
+        [InlineData("OptionNoValueElement", "Green")]
+        public void ValueElement_ShouldReturnCorrectValue(string id, string expected)
+        {
+            //arrange
+            var target = Target.The("value element").LocatedBy(By.Id(id));
+            var question = Value.Of(target).Value;
+            //act
+            var actual = Answer(question);
+            //assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ src/Tranquire.Selenium/Questions/Value.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace Tranquire.Selenium.Questions
+{
+    public class Value : SingleUIState<string, Value>
+    {
+        public Value(ITarget target) : base(target)
+        {
+        }
+
+        public Value(ITarget target, CultureInfo culture) : base(target, culture)
+        {
+        }
+
+        public static Value Of(ITarget target)
+        {
+            return new Value(target);
+        }
+
+        protected override Value CreateState(ITarget target, CultureInfo culture)
+        {
+            return new Value(target, culture);
+        }
+
+        protected override string ResolveFor(IWebElement element)
+        {
+            return element.GetAttribute("value");
+        }
+    }
+}
