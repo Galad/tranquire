ff537c663f5a11d8e5d370cae8d10a9c195c6b30
Lucas Loegel <lucas.loegel@outlook.com>
2016-04-03 17:07:19 +0200

    Add Enabled question

---
+++ src/Tranquire.Selenium.Tests/Questions.Enabled.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tranquire.Selenium.Questions;
+using Xunit;
+
+namespace Tranquire.Selenium.Tests
+{
+    public partial class Questions
+    {
+        [Theory]
+        [InlineData("EnabledElement", true)]
+        [InlineData("DisabledElement", false)]
+        [InlineData("CannotBeDisabledElement", true)]        
+        public void EnabledElement_ShouldReturnCorrectValue(string id, bool expected)
+        {
+            //arrange
+            var target = Target.The("enabled element").LocatedBy(By.Id(id));
+            var question = Enabled.Of(target).Value;
+            //act
+            var actual = Answer(question);
+            //assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ src/Tranquire.Selenium/Questions/Enabled.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace Tranquire.Selenium.Questions
+{
+    public class Enabled : SingleUIState<bool, Enabled>
+    {
+        public Enabled(ITarget target) : base(target)
+        {
+        }
+
+        public Enabled(ITarget target, CultureInfo culture) : base(target, culture)
+        {
+        }
+
+        public static Enabled Of(ITarget target)
+        {
+            return new Enabled(target);
+        }
+
+        protected override Enabled CreateState(ITarget target, CultureInfo culture)
+        {
+            return new Enabled(target, culture);
+        }
+
+        protected override bool ResolveFor(IWebElement element)
+        {
+            return element.Enabled;
+        }
+    }
+}
