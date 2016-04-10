9a502e875c1229c2f87c0bc354c5a8b9353d6efe
Lucas Loegel <lucas.loegel@outlook.com>
2016-04-10 14:56:00 +0200

    Add UsingIFrame action

---
+++ src/Tranquire.Selenium.Tests/UsingIFrameTests.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tranquire.Selenium.Actions;
+using Xunit;
+
+namespace Tranquire.Selenium.Tests
+{
+    public class UsingIFrameTests : WebDriverTest
+    {
+        public UsingIFrameTests(WebDriverFixture fixture) : base(fixture, "PageWithIFrame.html")
+        {
+        }
+
+        [Fact]
+        public void UsingIFrame_ShouldAllowToReachElementsInIFrame()
+        {
+            //arrange
+            var iframe = Target.The("iframe").LocatedBy(By.Id("IFrame"));
+            var expectedElement = Target.The("element in iframe").LocatedBy(By.Id("ElementInIFrame"));
+            //act
+            UsingIFrame.For(Fixture.Actor).LocatedBy(iframe);
+            var element = expectedElement.ResolveFor(Fixture.Actor);
+            //assert
+            Assert.NotNull(element);
+        }
+
+        [Fact]
+        public void UsingIFrame_WhenDisposed_ShouldAllowToReachElementsOutsideIFrame()
+        {
+            //arrange
+            var iframe = Target.The("iframe").LocatedBy(By.Id("IFrame"));
+            var expectedElement = Target.The("element outside iframe").LocatedBy(By.Id("ElementOutsideIFrame"));
+            //act
+            UsingIFrame.For(Fixture.Actor).LocatedBy(iframe).Dispose();
+            var element = expectedElement.ResolveFor(Fixture.Actor);
+            //assert
+            Assert.NotNull(element);
+        }
+    }
+}
---
+++ src/Tranquire.Selenium/Actions/UsingIFrame.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tranquire.Selenium.Actions
+{
+    /// <summary>
+    /// Allow to switch to a frame and go back to the parent frame
+    /// </summary>
+    public class UsingIFrame
+    {
+        private Actor _actor;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="UsingIFrame"/>
+        /// </summary>
+        /// <param name="actor">The actor used to retrieve the <see cref="BrowseTheWeb"/> ability</param>
+        public UsingIFrame(Actor actor)
+        {
+            Guard.ForNull(actor, "actor");
+            _actor = actor;
+        }
+
+        /// <summary>
+        /// Define the actor to use when switching to the frame
+        /// </summary>
+        /// <param name="actor"></param>
+        /// <returns>A <see cref="UsingIFrame"/> object that can be used to switch to a frame</returns>
+        public static UsingIFrame For(Actor actor)
+        {
+            return new UsingIFrame(actor);
+        }
+
+        /// <summary>
+        /// Switch to the frame located by the given <see cref="ITarget"/>
+        /// </summary>
+        /// <param name="target">The target representing the frame to switch to</param>
+        /// <returns>A <see cref="IDisposable"/> object. When it is disposed, the browser will go back to the parent frame.</returns>
+        public IDisposable LocatedBy(ITarget target)
+        {
+            var driver = _actor.BrowseTheWeb();
+            driver.SwitchTo().Frame(target.ResolveFor(_actor));
+            return new Disposable(() => driver.SwitchTo().ParentFrame());
+        }
+
+        private class Disposable : IDisposable
+        {
+            private System.Action _action;
+
+            public Disposable(System.Action action)
+            {
+                _action = action;
+            }
+
+            public void Dispose()
+            {
+                if(_action == null)
+                {
+                    return;
+                }
+                var action = Interlocked.Exchange(ref _action, null);
+                action();
+            }
+        }
+    }
+}
