271985f7610fb80a939dc19da8b41ea0fda3141c
Lucas Loegel <lucas.loegel@outlook.com>
2017-02-26 15:29:42 +0100

    Add Actions.FromResult factory method

---
+++ src/Tranquire.Tests/ActionsTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Ploeh.AutoFixture.Idioms;
+using Xunit;
+
+namespace Tranquire.Tests
+{
+    public class ActionsTests
+    {
+        [Theory, DomainAutoData]
+        public void Sut_VerifyGuardClauses(GuardClauseAssertion assertion)
+        {
+            assertion.Verify(typeof(Actions));
+        }
+
+        [Theory, DomainAutoData]
+        public void FromResult_WhenExecutingWhen_ShouldReturnCorrectValue(
+            IActor actor,
+            object expected
+            )
+        {
+            //arrange
+            var sut = Actions.FromResult(expected);
+            //act
+            var actual = sut.ExecuteWhenAs(actor);
+            //act
+            actual.Should().Be(expected);
+        }
+
+        [Theory, DomainAutoData]
+        public void FromResult_WhenExecutingGiven_ShouldReturnCorrectValue(
+            IActor actor,
+            object expected
+            )
+        {
+            //arrange
+            var sut = Actions.FromResult(expected);
+            //act
+            var actual = sut.ExecuteGivenAs(actor);
+            //act
+            actual.Should().Be(expected);
+        }
+
+        [Theory, DomainAutoData]
+        public void FromResult_NameShouldReturnCorrectValue(            
+            string value
+            )
+        {
+            //arrange
+            var sut = Actions.FromResult(value);
+            //act
+            var actual = sut.Name;
+            //act
+            var expected = "Returns " + value;
+            actual.Should().Be(expected);
+        }
+
+        [Theory, DomainAutoData]
+        public void FromResult_ToStringShouldBeName(            
+            string value
+            )
+        {
+            //arrange
+            var sut = Actions.FromResult(value);
+            //act
+            var actual = sut.ToString();
+            //act
+            var expected = sut.Name;
+            actual.Should().Be(expected);
+        }
+    }
+}
---
+++ src/Tranquire/Actions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tranquire
+{
+    /// <summary>
+    /// Contains utility functions for actions
+    /// </summary>
+    public static class Actions
+    {
+        private class ResultAction<T> : IAction<T>
+        {
+            private readonly T _result;
+
+            public ResultAction(T result)
+            {
+                _result = result;
+            }
+
+            public string Name => "Returns " + _result.ToString();
+
+            public T ExecuteGivenAs(IActor actor)
+            {
+                return _result;
+            }
+
+            public T ExecuteWhenAs(IActor actor)
+            {
+                return _result;
+            }
+
+            public override string ToString() => Name;
+        }
+
+        /// <summary>
+        /// Creates an action that returns the given value
+        /// </summary>
+        /// <typeparam name="TResult">The value type</typeparam>
+        /// <param name="result">The value returned by the action</param>
+        /// <returns></returns>
+        public static IAction<TResult> FromResult<TResult>(TResult result)
+        {
+            return new ResultAction<TResult>(result);
+        }
+    }
+}
