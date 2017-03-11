ce904b93fe146f3b9564685166ab04a70f2de933
Lucas Loegel <lucas.loegel@outlook.com>
2017-03-11 03:19:26 +0100

    Add abstract action with 2 abilities

--- src/Tranquire/Action.cs
+++ src/Tranquire/Action.cs
@@ -61,6 +61,63 @@
 
     /// <summary>
     /// Represent an action on the system
+    /// </summary>    
+    /// <typeparam name="TGiven">The ability required for the Given context</typeparam>
+    /// <typeparam name="TWhen">The ability required for the When context</typeparam>
+    /// <typeparam name="TResult">The type returned by the action. Use the <see cref="Unit"/> to represent void actions</typeparam>
+    public abstract class Action<TGiven, TWhen, TResult> : IAction<TGiven, TWhen, TResult>
+    {
+        /// <summary>
+        /// Executes the action
+        /// </summary>
+        /// <param name="actor">The actor used to execute the action.</param>
+        /// <param name="ability">The ability for the current action.</param>
+        public TResult ExecuteGivenAs(IActor actor, TGiven ability)
+        {
+            Guard.ForNull(actor, nameof(actor));
+            Guard.ForNull(ability, nameof(ability));
+            return ExecuteGiven(actor, ability);
+        }
+
+        /// <summary>
+        /// Executes the action
+        /// </summary>
+        /// <param name="actor">The actor used to execute the action.</param>
+        /// <param name="ability">The ability for the current action.</param>
+        public TResult ExecuteWhenAs(IActor actor, TWhen ability)
+        {
+            Guard.ForNull(actor, nameof(actor));
+            Guard.ForNull(ability, nameof(ability));
+            return ExecuteWhen(actor, ability);
+        }
+
+        /// <summary>
+        /// Executes the action when the method when the execution is in a When context
+        /// </summary>
+        /// <param name="actor">The actor used to execute the action. Cannot be null.</param>
+        /// <param name="ability">The ability for the current action. Cannot be null.</param>
+        protected abstract TResult ExecuteWhen(IActor actor, TWhen ability);
+
+        /// <summary>
+        /// Executes the action when the method when the execution is in a Given context.
+        /// </summary>
+        /// <param name="actor">The actor used to execute the action. Cannot be null.</param>
+        /// <param name="ability">The ability for the current action. Cannot be null.</param>
+        protected abstract TResult ExecuteGiven(IActor actor, TGiven ability);
+
+        /// <summary>
+        /// Gets the action's name
+        /// </summary>
+        public abstract string Name { get; }
+        /// <summary>
+        /// Returns the action's <see cref="Name"/>
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() => Name;
+    }
+
+    /// <summary>
+    /// Represent an action on the system
     /// </summary>
     /// <typeparam name="TResult">The type returned by the action. Use the <see cref="Unit"/> to represent void actions</typeparam>
     public abstract class Action<TResult> : IAction<TResult>
