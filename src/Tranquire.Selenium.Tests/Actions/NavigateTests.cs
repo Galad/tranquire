72f6c307fb4dc291d10919a07d0e224a94c24f0f
Lucas Loegel <lucas.loegel@outlook.com>
2017-03-16 00:11:00 +0100

    Add Navigate action

---
+++ src/Tranquire.Selenium.Tests/Actions/NavigateTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using OpenQA.Selenium;
+using Ploeh.AutoFixture.Idioms;
+using Ploeh.AutoFixture.Xunit2;
+using Tranquire.Selenium.Actions;
+using Tranquire.Tests;
+using Xunit;
+
+namespace Tranquire.Selenium.Tests.Actions
+{
+    public class NavigateTests
+    {
+        [Theory, DomainAutoData]
+        public void Sut_VerifyGuardClauses(GuardClauseAssertion assertion)
+        {
+            assertion.Verify(typeof(Navigate));
+        }
+
+        [Theory, DomainAutoData]
+        public void To_ShouldReturnCorrectValue(Uri uri)
+        {
+            var actual = Navigate.To(uri);
+            actual.ShouldBeEquivalentTo(new Navigate(uri));
+        }
+
+        [Theory, DomainAutoData]
+        public void Name_ShouldReturnCorrectValue(Navigate sut)
+        {
+            sut.Name.Should().Be($"Navigate to {sut.Uri}");
+        }
+        
+        [Theory, DomainAutoData]
+        public void ExecuteGivenAs_ShouldReturnCorrectValue(            
+            Navigate sut,
+            [Frozen]IWebDriver webDriver,
+            BrowseTheWeb ability,
+            Mock<INavigation> navigation,
+            IActor actor)
+        {
+            //arrange
+            Mock.Get(webDriver).Setup(w => w.Navigate()).Returns(navigation.Object);
+            //act
+            sut.ExecuteGivenAs(actor, ability);
+            //assert
+            navigation.Verify(n => n.GoToUrl(sut.Uri));
+        }
+
+        [Theory, DomainAutoData]
+        public void ExecuteWhenAs_ShouldReturnCorrectValue(
+            Navigate sut,
+            [Frozen]IWebDriver webDriver,
+            BrowseTheWeb ability,
+            Mock<INavigation> navigation,
+            IActor actor)
+        {
+            //arrange
+            Mock.Get(webDriver).Setup(w => w.Navigate()).Returns(navigation.Object);
+            //act
+            sut.ExecuteWhenAs(actor, ability);
+            //assert
+            navigation.Verify(n => n.GoToUrl(sut.Uri));
+        }
+    }
+}
---
+++ src/Tranquire.Selenium/Actions/Navigate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tranquire.Selenium.Actions
+{
+    /// <summary>
+    /// Navigate to an url
+    /// </summary>
+    public sealed class Navigate : ActionUnit<BrowseTheWeb>
+    {
+        /// <summary>
+        /// Gets the Uri to navigate to 
+        /// </summary>
+        public Uri Uri { get; }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Navigate"/>
+        /// </summary>
+        /// <param name="uri">The uri to navigate to</param>
+        public Navigate(Uri uri)
+        {
+            Uri = uri ?? throw new ArgumentNullException(nameof(uri));
+        }
+
+        /// <summary>
+        /// Creates an action that navigates to an Uri
+        /// </summary>
+        /// <param name="uri">The Uri to navigate to</param>
+        /// <returns>A new action that navigate to the Uri</returns>
+        public static IAction<BrowseTheWeb, BrowseTheWeb, Unit> To(Uri uri)
+        {
+            return new Navigate(uri);
+        }
+
+        /// <summary>
+        /// Execute the action
+        /// </summary>
+        /// <param name="actor"></param>
+        /// <param name="ability"></param>
+        protected override void ExecuteWhen(IActor actor, BrowseTheWeb ability)
+        {
+            if (ability == null) throw new ArgumentNullException(nameof(ability));
+            ability.Navigate().GoToUrl(Uri);
+        }
+
+        /// <summary>
+        /// Gets the action name
+        /// </summary>
+        public override string Name => "Navigate to " + Uri.ToString();
+    }
+}
