9de48f42e58218482aa62d9a13c8e025555b8634
Lucas Loegel <lucas.loegel@outlook.com>
2017-03-16 02:49:47 +0100

    Add RightClick Selenium action

---
+++ src/Tranquire.Selenium.Tests/Actions/OpenContextMenuTests.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tranquire.Selenium.Actions;
+using Tranquire.Selenium.Questions;
+using Tranquire.Tests;
+using Xunit;
+
+namespace Tranquire.Selenium.Tests.Actions
+{
+    public class OpenContextMenuTests : WebDriverTest
+    {
+        public OpenContextMenuTests(WebDriverFixture fixture) : base(fixture, "OpenContextMenu.html")
+        {
+        }
+
+        [Theory, DomainAutoData]
+        public void Execute_WhenTimeoutExpires_ShouldThrow(string expected)
+        {
+            //arrange
+            var js = $"setupTest('{expected}');";
+            Fixture.WebDriver.ExecuteScript(js);
+            var target = Target.The("element where to open the context menu").LocatedBy(By.Id("ClickableElement"));
+            var expectedClickContent = Target.The("expected click content").LocatedBy(By.Id("ExpectedValue"));
+            var action = OpenContextMenu.On(target);
+            //act
+            Fixture.Actor.When(action);
+            //assert
+            var actual = Answer(Value.Of(expectedClickContent).Value);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ src/Tranquire.Selenium/Actions/OpenContextMenu.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace Tranquire.Selenium.Actions
+{
+    /// <summary>
+    /// Opens the context menu on a target
+    /// </summary>
+    public sealed class OpenContextMenu : ActionUnit<WebBrowser>
+    {
+        private readonly ITarget _target;
+
+        /// <summary>
+        /// Gets the action name
+        /// </summary>
+        public override string Name => $"Right click on {_target.Name}";
+
+        /// <summary>
+        /// Creates a new instance of <see cref="OpenContextMenu"/>
+        /// </summary>
+        /// <param name="target">The element where the action should be performed</param>
+        public OpenContextMenu(ITarget target)
+        {
+            _target = target ?? throw new ArgumentNullException(nameof(target));
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="OpenContextMenu"/> action
+        /// </summary>
+        /// <param name="target">The element where the action should be performed</param>
+        /// <returns></returns>
+        public static IAction<Unit> On(ITarget target)
+        {
+            return new OpenContextMenu(target);
+        }
+
+        /// <summary>
+        /// Execute the action
+        /// </summary>
+        /// <param name="actor"></param>
+        /// <param name="ability"></param>
+        protected override void ExecuteWhen(IActor actor, WebBrowser ability)
+        {
+            var seleniumActions = new OpenQA.Selenium.Interactions.Actions(ability.Driver);
+            var element = _target.ResolveFor(ability.Driver);
+            seleniumActions.ContextClick(element)
+                           .Build()
+                           .Perform();
+        }
+    }
+}
