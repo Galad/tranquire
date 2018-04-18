46e967707dfb8e166cd18d07295c8d8cd999a351
Lucas Loegel <lucas.loegel@enablon.com>
2018-04-18 10:32:36 +0200

    Add DefaultCompositeAction

---
+++ src/Tranquire/DefaultCompositeAction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tranquire
+{
+    /// <summary>
+    /// Represent a composite action that can be instanciated
+    /// </summary>
+    public class DefaultCompositeAction : CompositeAction
+    {
+        /// <summary>
+        /// Creates a new instance of  <see cref="DefaultCompositeAction"/>
+        /// </summary>
+        /// <param name="name">The action name.</param>
+        /// <param name="actions">The actions to execute.</param>
+        public DefaultCompositeAction(string name, params IAction<Unit>[] actions) : base(actions)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            Name = name;
+        }
+
+        /// <inheritsdoc />
+        public override string Name { get; }
+    }
+}
