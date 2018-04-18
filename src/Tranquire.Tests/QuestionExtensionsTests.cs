338ab623663300cad92c4183e1cf982e0d8ee855
Lucas Loegel <lucas.loegel@enablon.com>
2018-04-18 11:42:53 +0200

    Add question extension method Select

---
+++ src/Tranquire.Tests/QuestionExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Ploeh.AutoFixture.Idioms;
+using System;
+using Tranquire.Extensions;
+using Xunit;
+
+namespace Tranquire.Tests
+{
+    public class QuestionExtensionsTests
+    {
+        [Theory, DomainAutoData]
+        public void Sut_VerifyGuardClauses(GuardClauseAssertion assertion) => assertion.Verify(typeof(QuestionExtensions));
+
+        [Theory, DomainAutoData]
+        public void Select_ShouldReturnCorrectResult(IQuestion<string> question, Func<string, object> selector)
+        {
+            // act
+            var actual = QuestionExtensions.Select(question, selector);
+            // assert
+            var expected = new SelectQuestion<string, object>(question, selector);
+            actual.Should().BeOfType<SelectQuestion<string, object>>();
+            actual.ShouldBeEquivalentTo(expected);
+        }
+    }
+}
