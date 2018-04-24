617333b335d201133ba88a360cf75859b9096fb9
Lucas Loegel <galad@outlook.com>
2018-04-24 22:30:43 +0200

    Give the ability to specify a format for screenshots names (#17) (#18)

--- src/Tranquire.Selenium/ActorExtensions.cs
+++ src/Tranquire.Selenium/ActorExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
 using System.Threading;
 
 namespace Tranquire.Selenium
@@ -31,25 +33,39 @@ namespace Tranquire.Selenium
             return new Actor(actor.Name, actor.Abilities, a => new SlowSelenium(actor.InnerActorBuilder(a), delay));
         }
 
+        //private static readonly Regex hasFormatItem = new Regex("\{")
         /// <summary>
         /// Add the ability to take screenshot on each action
         /// </summary>
         /// <param name="actor">The actor</param>
         /// <param name="directory">The directory where the screenshots will be saved</param>
-        /// <param name="screenshotName">A function that generate a new name of the screenshot</param>
+        /// <param name="screenshotNameOrFormat">A string containing a format for 0 or 1 format item, used to generate the screenshot names. If no format item is provided, the default format is "<paramref name="screenshotNameOrFormat"/>_{0:00}".</param>
+        /// <example>
+        /// actor.TakeScreenshots(@"C:\tests\screeshots", "Screenhshot_{0:000}")
+        /// </example>
         /// <returns>An new actor taking screenshots</returns>
-        public static Actor TakeScreenshots(this Actor actor, string directory, string screenshotName)
+        public static Actor TakeScreenshots(this Actor actor, string directory, string screenshotNameOrFormat)
         {
             Guard.ForNull(actor, nameof(actor));
             Guard.ForNull(directory, nameof(directory));
-            Guard.ForNull(screenshotName, nameof(screenshotName));
+            Guard.ForNull(screenshotNameOrFormat, nameof(screenshotNameOrFormat));
             var id = 0;
-            return new Actor(actor.Name, actor.Abilities, a => new TakeScreenshot(actor.InnerActorBuilder(a), directory, () => NextScreenshotName(ref id, screenshotName)));
-        }
 
-        private static string NextScreenshotName(ref int i, string screenshotName)
-        {
-            return $"{screenshotName}_{Interlocked.Increment(ref i)}.jpg";
+            string screenshotFormat;
+            if (string.Format(screenshotNameOrFormat, 1) == screenshotNameOrFormat)
+            {
+                screenshotFormat = screenshotNameOrFormat + "_" + "{0:00}"; 
+            }
+            else
+            {
+                screenshotFormat = screenshotNameOrFormat;
+            }
+
+            string nextScreenshotName()
+            {
+                return string.Format(CultureInfo.InvariantCulture, screenshotFormat, Interlocked.Increment(ref id));
+            }
+            return new Actor(actor.Name, actor.Abilities, a => new TakeScreenshot(actor.InnerActorBuilder(a), directory, nextScreenshotName));
         }
     }
 }
