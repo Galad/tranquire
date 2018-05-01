cb1bc4b45a0285647fe1fbd7c6e9a7fb121eeabc
Lucas Loegel <lucas.loegel@outlook.com>
2018-05-01 12:31:53 +0200

    Add DebugObserver

--- src/Tranquire.Tests/Reporting/ConsoleObserverTests.cs
+++ src/Tranquire.Tests/Reporting/ConsoleObserverTests.cs
@@ -1,80 +1,28 @@
 using Moq;
 using System;
 using Tranquire.Reporting;
-using Xunit;
 using AutoFixture;
-using AutoFixture.Idioms;
 using System.IO;
 using System.Text;
 
 namespace Tranquire.Tests.Reporting
 {
-    public class ConsoleObserverTests : IDisposable
+    public class ConsoleObserverTests : WriteStringObserverTestsBase<ConsoleObserver>
     {
-        private readonly StringBuilder _out;
         private readonly TextWriter _standardOut;
 
-        public ConsoleObserverTests()
+        public ConsoleObserverTests() : this(new StringBuilder())
         {
-            _out = new StringBuilder();
+
+        }
+
+        private ConsoleObserverTests(StringBuilder @out) : base(@out)
+        {
             _standardOut = Console.Out;
-            Console.SetOut(new StringWriter(_out));
+            Console.SetOut(new StringWriter(@out));
         }
 
-        [Theory, DomainAutoData]
-        public void Sut_VerifyGuardClauses(GuardClauseAssertion assertion)
-        {
-            assertion.Verify(typeof(ConsoleObserver));
-        }
-
-        [Theory, DomainAutoData]
-        public void OnComplete_ShouldWriteCorrectValue(
-            ConsoleObserver sut)
-        {
-            // act
-            sut.OnCompleted();
-            // assert
-            Assert.Equal("Completed\r\n", _out.ToString());
-        }
-
-        [Theory, DomainAutoData]
-        public void OnError_ShouldWriteCorrectValue(
-            ConsoleObserver sut,
-            Exception exception)
-        {
-            // act
-            sut.OnError(exception);
-            // assert
-            Assert.Equal("Error: " + exception.Message + "\r\n" + exception.StackTrace + "\r\n", _out.ToString());
-        }
-
-        [Theory, DomainAutoData]
-        public void OnNext_ShouldWriteCorrectValue(
-            ConsoleObserver sut,
-            string expected)
-        {
-            // act
-            sut.OnNext(expected);
-            // assert
-            Assert.Equal(expected + "\r\n", _out.ToString());
-        }
-
-        [Theory, DomainAutoData]
-        public void OnNext_MultipleTimes_ShouldWriteCorrectValue(
-            ConsoleObserver sut,
-            string[] values)
-        {
-            // act
-            foreach (var value in values)
-            {
-                sut.OnNext(value);
-            }
-            // assert
-            var expected = string.Join("\r\n", values);
-            Assert.Equal(expected + "\r\n", _out.ToString());
-        }
-
-        public void Dispose()
+        public override void Dispose()
         {
             Console.SetOut(_standardOut);
         }
