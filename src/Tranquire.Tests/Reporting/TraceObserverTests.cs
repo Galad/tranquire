48cf92c85dbf9bce8c39b5d382674086703698fd
Lucas Loegel <lucas.loegel@outlook.com>
2018-05-01 12:41:01 +0200

    Add TraceObserver

---
+++ src/Tranquire.Tests/Reporting/TraceObserverTests.cs
@@ -0,0 +1,27 @@
+#if NET462
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using Tranquire.Reporting;
+
+namespace Tranquire.Tests.Reporting
+{
+    public class TraceObserverTests : WriteStringObserverTestsBase<TraceObserver>
+    {
+        private readonly int _position;
+
+        public TraceObserverTests() : this(new StringBuilder()) { }
+
+        private TraceObserverTests(StringBuilder @out) : base(@out)
+        {
+            _position = Trace.Listeners.Add(new TextWriterTraceListener(new StringWriter(@out)));
+        }
+
+        public override void Dispose()
+        {
+            Trace.Listeners.RemoveAt(_position);
+            base.Dispose();
+        }
+    }
+}
+#endif
