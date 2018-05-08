d537a24a7c1e87bb42c83e211e05d784f06829a0
Lucas Loegel <galad@outlook.com>
2018-05-08 17:57:14 +0200

    Add ScreenshotInfoToActionAttachmentObserverAdapter (#29)
    #23

---
+++ src/Tranquire.Selenium.Tests/Extensions/ScreenshotInfoToActionAttachmentObserverTests.cs
@@ -0,0 +1,76 @@
+using AutoFixture;
+using AutoFixture.Idioms;
+using FluentAssertions;
+using Moq;
+using System;
+using System.IO;
+using Tranquire.Reporting;
+using Tranquire.Selenium.Extensions;
+using Tranquire.Tests;
+using Xunit;
+
+namespace Tranquire.Selenium.Tests.Extensions
+{
+    public class ScreenshotInfoToActionAttachmentObserverAdapterTests : WebDriverTest
+    {
+        public ScreenshotInfoToActionAttachmentObserverAdapterTests(WebDriverFixture fixture) : base(fixture, "Actions.html")
+        {
+            
+        }
+
+        [Theory, DomainAutoData]
+        public void Sut_VerifyGuardClause(GuardClauseAssertion assertion, IFixture fixture)
+        {
+            fixture.Register(() => Fixture.WebDriver.GetScreenshot());
+            assertion.Verify(typeof(ScreenshotInfoToActionAttachmentObserverAdapter));
+        }
+
+        [Theory, DomainAutoData]
+        public void Sut_ShouldBeObserverOfScreenshotInfo(ScreenshotInfoToActionAttachmentObserverAdapter sut)
+        {
+            sut.Should().BeAssignableTo<IObserver<ScreenshotInfo>>();
+        }
+
+        [Theory, DomainAutoData]
+        public void Sut_ShouldInitializeObserver(ConstructorInitializedMemberAssertion assertion)
+        {
+            assertion.Verify<ScreenshotInfoToActionAttachmentObserverAdapter>(o => o.AttachmentObserver);
+        }
+
+        [Theory, DomainAutoData]
+        public void OnCompleted_ShouldCallOnCompleted(
+            ScreenshotInfoToActionAttachmentObserverAdapter sut)
+        {
+            // act
+            sut.OnCompleted();
+            // assert
+            Mock.Get(sut.AttachmentObserver).Verify(o => o.OnCompleted());
+        }
+        
+        [Theory, DomainAutoData]
+        public void OnError_ShouldCallOnError(
+            ScreenshotInfoToActionAttachmentObserverAdapter sut, Exception exception)
+        {
+            // act
+            sut.OnError(exception);
+            // assert
+            Mock.Get(sut.AttachmentObserver).Verify(o => o.OnError(exception));
+        }
+
+        [Theory, DomainAutoData]
+        public void OnNext_ShouldCallOnNext(
+            ScreenshotInfoToActionAttachmentObserverAdapter sut,
+            string filename)
+        {
+            // arrange
+            var screenshotInfo = new ScreenshotInfo(Fixture.WebDriver.GetScreenshot(), filename);
+            // act
+            sut.OnNext(screenshotInfo);
+            // assert
+            var expected = new ActionFileAttachment(filename, string.Empty);
+            Mock.Get(sut.AttachmentObserver).Verify(o => o.OnNext(It.Is<ActionFileAttachment>(a => 
+                    a.FilePath == filename + ".jpg" &&
+                    a.Description == string.Empty)));
+        }
+    }
+}
---
+++ src/Tranquire.Selenium/Extensions/ScreenshotInfoToActionAttachmentObserver.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tranquire.Reporting;
+
+namespace Tranquire.Selenium.Extensions
+{
+    /// <summary>
+    /// Represent an observer that adapts the <see cref="ScreenshotInfo"/> notifications to a <see cref="ActionFileAttachment"/> notification that is passed to the new observer
+    /// </summary>
+    public sealed class ScreenshotInfoToActionAttachmentObserverAdapter : IObserver<ScreenshotInfo>
+    {
+        /// <summary>
+        /// Gets the attachment observer to adapt the notifications
+        /// </summary>
+        public IObserver<ActionFileAttachment> AttachmentObserver { get; }
+
+        /// <summary>
+        /// Creates a new instance of<see cref="ScreenshotInfoToActionAttachmentObserverAdapter"/>
+        /// </summary>
+        /// <param name="attachmentObserver"></param>
+        public ScreenshotInfoToActionAttachmentObserverAdapter(IObserver<ActionFileAttachment> attachmentObserver)
+        {
+            AttachmentObserver = attachmentObserver ?? throw new ArgumentNullException(nameof(attachmentObserver));
+        }
+
+        /// <inheritdoc />
+        public void OnCompleted()
+        {
+            AttachmentObserver.OnCompleted();
+        }
+
+        /// <inheritdoc />
+        public void OnError(Exception error)
+        {
+            if (error == null)
+            {
+                throw new ArgumentNullException(nameof(error));
+            }
+
+            AttachmentObserver.OnError(error);
+        }
+
+        /// <inheritdoc />
+        public void OnNext(ScreenshotInfo value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            AttachmentObserver.OnNext(new ActionFileAttachment(value.FileName + ".jpg", string.Empty));
+        }
+    }
+}
