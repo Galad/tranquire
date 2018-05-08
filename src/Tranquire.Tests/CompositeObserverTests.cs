05d4ecb53379e6b22e230f8bb516e7de8e36f1b3
Lucas Loegel <galad@outlook.com>
2018-05-08 10:16:13 +0200

    Add composite observer

---
+++ src/Tranquire.Tests/CompositeObserverTests.cs
@@ -0,0 +1,69 @@
+using AutoFixture;
+using AutoFixture.Idioms;
+using Moq;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Tranquire.Tests
+{
+    public class CompositeObserverTests
+    {
+        [Theory, DomainAutoData]
+        public void Sut_VerifyGuardClause(GuardClauseAssertion assertion)
+        {
+            assertion.Verify(typeof(CompositeObserver<object>).GetConstructors());
+        }
+
+        [Theory]
+        [DomainInlineAutoData(0)]
+        [DomainInlineAutoData(1)]
+        [DomainInlineAutoData(2)]
+        [DomainInlineAutoData(10)]
+        public void Sut_ShouldBeEnumerableOfObservers(int count, IFixture fixture)
+        {
+            // arrange
+            var observers = fixture.CreateMany<IObserver<object>>(count).ToArray();
+            fixture.Inject(observers);
+            var sut = fixture.Create<CompositeObserver<object>>();
+            // act and assert
+            Assert.Equal(observers, sut);
+        }
+
+        [Theory, DomainAutoData]
+        public void OnCompleted_ShouldCallOnCompletedOnAllObservers(CompositeObserver<object> sut)
+        {
+            // act
+            sut.OnCompleted();
+            // assert
+            foreach (var mock in sut.Select(Mock.Get))
+            {
+                mock.Verify(o => o.OnCompleted());
+            }
+        }
+        
+        [Theory, DomainAutoData]
+        public void OnError_ShouldCallOnErrorOnAllObservers(CompositeObserver<object> sut, Exception exception)
+        {
+            // act
+            sut.OnError(exception);
+            // assert
+            foreach (var mock in sut.Select(Mock.Get))
+            {
+                mock.Verify(o => o.OnError(exception));
+            }
+        }
+
+        [Theory, DomainAutoData]
+        public void OnNext_ShouldCallOnNextOnAllObservers(CompositeObserver<object> sut, object value)
+        {
+            // act
+            sut.OnNext(value);
+            // assert
+            foreach (var mock in sut.Select(Mock.Get))
+            {
+                mock.Verify(o => o.OnNext(value));
+            }
+        }
+    }
+}
---
+++ src/Tranquire/CompositeObserver.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tranquire
+{
+    /// <summary>
+    /// A composite implementation of <see cref="IObserver{T}"/> 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class CompositeObserver<T> : IEnumerable<IObserver<T>>, IObserver<T>
+    {
+        private readonly IEnumerable<IObserver<T>> _observers;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="CompositeObserver{T}"/>
+        /// </summary>
+        /// <param name="observers"></param>
+        public CompositeObserver(params IObserver<T>[] observers)
+        {
+            this._observers = observers ?? throw new ArgumentNullException(nameof(observers));
+        }
+
+        /// <inheritdoc />
+        public void OnCompleted()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.OnCompleted();
+            }
+        }
+
+        /// <inheritdoc />
+        public void OnError(Exception error)
+        {
+            foreach (var observer in _observers)
+            {
+                observer.OnError(error);
+            }            
+        }
+
+        /// <inheritdoc />
+        public void OnNext(T value)
+        {
+            foreach (var observer in _observers)
+            {
+                observer.OnNext(value);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _observers.GetEnumerator();
+        }
+
+        /// <inheritdoc />
+        public IEnumerator<IObserver<T>> GetEnumerator()
+        {
+            return _observers.GetEnumerator();
+        }
+    }
+}
