fc879634d0de23cc636b54399adf8187c9203b7b
Lucas Loegel <galad@outlook.com>
2018-05-25 00:22:52 +0200

    Add question from a func

---
+++ src/Tranquire/Questions.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tranquire
+{
+    /// <summary>
+    /// Contains utility functions for questions
+    /// </summary>
+    public static class Questions
+    {
+        /// <summary>
+        /// Creates a question that executes the given function and returns its result
+        /// </summary>
+        /// <param name="name">The action name</param>
+        /// <param name="func">The function to execute</param>
+        /// <returns></returns>
+        public static IQuestion<TAnswer> Create<TAnswer>(string name, Func<IActor, TAnswer> func)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+            if (func == null)
+            {
+                throw new ArgumentNullException(nameof(func));
+            }
+
+            return new FuncQuestions<TAnswer>(name, func);
+        }
+
+        private class FuncQuestions<TAnswer> : Question<TAnswer>
+        {
+            private readonly string name;
+            private readonly Func<IActor, TAnswer> func;
+
+            public FuncQuestions(string name, Func<IActor, TAnswer> func)
+            {
+                this.name = name;
+                this.func = func;
+            }
+
+            public override string Name => name;
+
+            protected override TAnswer Answer(IActor actor)
+            {
+                return func(actor);
+            }
+        }
+
+        /// <summary>
+        /// Creates a question that executes the given function and returns its result
+        /// </summary>
+        /// <param name="name">The action name</param>
+        /// <param name="func">The function to execute</param>
+        /// <returns></returns>
+#pragma warning disable CS0618 // Type or member is obsolete
+        public static IQuestion<TAnswer, TAbility> Create<TAnswer, TAbility>(string name, Func<IActor, TAbility, TAnswer> func)
+#pragma warning restore CS0618 // Type or member is obsolete
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+            if (func == null)
+            {
+                throw new ArgumentNullException(nameof(func));
+            }
+
+            return new FuncQuestions<TAnswer, TAbility>(name, func);
+        }
+
+        private class FuncQuestions<TAnswer, TAbility> : Question<TAnswer, TAbility>
+        {
+            private readonly string name;
+            private readonly Func<IActor, TAbility, TAnswer> func;
+
+            public FuncQuestions(string name, Func<IActor, TAbility, TAnswer> func)
+            {
+                this.name = name;
+                this.func = func;
+            }
+
+            public override string Name => name;
+
+            protected override TAnswer Answer(IActor actor, TAbility ability)
+            {
+                return func(actor, ability);
+            }
+        }
+    }
+}
