cb0b07af1fbec0592521e9d2a3ef9744c60419c6
Lucas Loegel <galad@outlook.com>
2018-06-02 21:45:36 +0200

    Test without nunit package

--- src/Tranquire.NUnit/VerifiesExtensions.cs
+++ src/Tranquire.NUnit/VerifiesExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using NUnit.Framework;
-using NUnit.Framework.Constraints;
 
 namespace Tranquire
 {
@@ -12,105 +10,14 @@ namespace Tranquire
     public static class VerifiesExtensions
     {
         /// <summary>
-        /// Verifies the answer of the given question using a NUnit constraint
+        /// .
         /// </summary>
-        /// <typeparam name="T">The answer type</typeparam>
-        /// /// <param name="verifies">The <see cref="IVerifies"/> instance</param>
-        /// <param name="question">The question to verify the answer from</param>
-        /// <param name="constraint">A NUnit constraint that encapsulate the expected result</param>        
-        /// <returns>The answer, when the verification succeeds</returns>
-        public static T Then<T>(IVerifies verifies, IQuestion<T> question, IResolveConstraint constraint)
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static string Test(this IVerifies v)
         {
-            if (verifies == null)
-            {
-                throw new ArgumentNullException(nameof(verifies));
-            }
-            if (question == null)
-            {
-                throw new ArgumentNullException(nameof(question));
-            }
-            if (constraint == null)
-            {
-                throw new ArgumentNullException(nameof(constraint));
-            }
-
-            return verifies.Then(question, answer => Assert.That(answer, constraint));
-        }
-
-        /// <summary>
-        /// Verifies the answer of the given question using a NUnit constraint
-        /// </summary>
-        /// <typeparam name="T">The answer type</typeparam>
-        /// /// <param name="verifies">The <see cref="IVerifies"/> instance</param>
-        /// <param name="question">The question to verify the answer from</param>
-        /// <param name="constraint">A NUnit constraint that encapsulate the expected result</param>
-        /// <param name="getExceptionMessage">A function that returns the message to return when the assertion fails</param>        
-        /// <returns>The answer, when the verification succeeds</returns>
-        public static T Then<T>(
-            IVerifies verifies, 
-            IQuestion<T> question, 
-            IResolveConstraint constraint,
-            Func<string> getExceptionMessage)
-        {
-            if (verifies == null)
-            {
-                throw new ArgumentNullException(nameof(verifies));
-            }
-            if (question == null)
-            {
-                throw new ArgumentNullException(nameof(question));
-            }
-            if (constraint == null)
-            {
-                throw new ArgumentNullException(nameof(constraint));
-            }
-            if (getExceptionMessage == null)
-            {
-                throw new ArgumentNullException(nameof(getExceptionMessage));
-            }
-
-            return verifies.Then(question, answer => Assert.That(answer, constraint, getExceptionMessage));
-        }
-
-        /// <summary>
-        /// Verifies the answer of the given question using a NUnit constraint
-        /// </summary>
-        /// <typeparam name="T">The answer type</typeparam>
-        /// /// <param name="verifies">The <see cref="IVerifies"/> instance</param>
-        /// <param name="question">The question to verify the answer from</param>
-        /// <param name="constraint">A NUnit constraint that encapsulate the expected result</param>
-        /// <param name="message">The message to return when the assertion fails</param>
-        /// <param name="args">Arguments to add to the message</param>
-        /// <returns>The answer, when the verification succeeds</returns>
-        public static T Then<T>(
-            IVerifies verifies,
-            IQuestion<T> question,
-            IResolveConstraint constraint,
-            string message,
-            params object[] args)
-        {
-            if (verifies == null)
-            {
-                throw new ArgumentNullException(nameof(verifies));
-            }
-            if (question == null)
-            {
-                throw new ArgumentNullException(nameof(question));
-            }
-            if (constraint == null)
-            {
-                throw new ArgumentNullException(nameof(constraint));
-            }
-            if (message == null)
-            {
-                throw new ArgumentNullException(nameof(message));
-            }
-            if (args == null)
-            {
-                throw new ArgumentNullException(nameof(args));
-            }
-
-            return verifies.Then(question, answer => Assert.That(answer, constraint, message, args));
+            // .
+            return "test";
         }
     }
 }
