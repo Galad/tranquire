f0c4d481f588c3218d51b9f41002075b976b2952
Lucas Loegel <galad@outlook.com>
2018-08-20 09:28:24 +0200

    Change path of content file

--- src/Tranquire.Selenium.Tests/Questions/HtmlTests.cs
+++ src/Tranquire.Selenium.Tests/Questions/HtmlTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Reflection;
 using Tranquire.Selenium.Questions;
 using Xunit;
 
@@ -14,6 +15,7 @@ namespace Tranquire.Selenium.Tests.Questions
         public void HtmlOfPage_ShouldReturnCorrectValue()
         {
             // arrange
+            var filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase), "Html.html");
             var expected = File.ReadAllText("Html.html").Replace("\r\n", string.Empty);
             // act
             var actual = Answer(Html.OfPage);
