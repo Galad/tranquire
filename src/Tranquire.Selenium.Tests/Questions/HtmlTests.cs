b87671cd87f64fa624b4518f4a1b2cca7179eef0
Lucas Loegel <galad@outlook.com>
2018-08-20 09:03:34 +0200

    Add Html Question

---
+++ src/Tranquire.Selenium.Tests/Questions/HtmlTests.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using Tranquire.Selenium.Questions;
+using Xunit;
+
+namespace Tranquire.Selenium.Tests.Questions
+{
+    public class HtmlTests : WebDriverTest
+    {
+        public HtmlTests(WebDriverFixture fixture) : base(fixture, "Html.html")
+        {
+        }
+
+        [Fact]
+        public void HtmlOfPage_ShouldReturnCorrectValue()
+        {
+            // arrange
+            var expected = File.ReadAllText("Html.html").Replace("\r\n", string.Empty);
+            // act
+            var actual = Answer(Html.OfPage);
+            // assert
+            Assert.Equal(expected, actual.Replace("\r\n", string.Empty));
+        }
+    }
+}
---
+++ src/Tranquire.Selenium/Questions/Html.cs
@@ -0,0 +1,22 @@
+﻿namespace Tranquire.Selenium.Questions
+{
+    /// <summary>
+    /// Provide Html for a WebBrowser
+    /// </summary>
+    public class Html : Question<string, WebBrowser>
+    {
+        /// <inheritsdoc />
+        public override string Name => "What is the current page HTML ?";
+
+        /// <inheritsdoc />
+        protected override string Answer(IActor actor, WebBrowser ability)
+        {
+            return ability.Driver.PageSource;
+        }
+
+        /// <summary>
+        /// Gets a question that returns the Page html
+        /// </summary>
+        public static IQuestion<string> OfPage { get; } = new Html();
+    }
+}
