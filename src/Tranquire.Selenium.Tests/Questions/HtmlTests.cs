b9795fa7ca49169ce9636d39f104cf359b6b643b
Lucas Loegel <galad@outlook.com>
2018-08-28 14:31:58 +0200

    Add Html Question (#79)
    Close #74

---
+++ src/Tranquire.Selenium.Tests/Questions/HtmlTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Tranquire.Selenium.Questions;
+using Xunit;
+
+namespace Tranquire.Selenium.Tests.Questions
+{
+    public class HtmlTests : WebDriverTest
+    {
+        public HtmlTests(WebDriverFixture fixture) : base(fixture, "Html.html")
+        {
+        }
+
+        [Fact]
+        public async Task HtmlOfPage_ShouldReturnCorrectValue()
+        {
+            // arrange
+            var expected = normalizeHtml(await GetExpectedHtml()); 
+            // act
+            var actual = Answer(Html.OfPage);
+            // assert
+            Assert.Equal(expected, normalizeHtml(actual));
+
+            string normalizeHtml(string html)
+            {
+                return html.Replace("\r\n", string.Empty)
+                           .Replace("\n", string.Empty)
+                           .Replace("\r", string.Empty);
+            }
+        }
+
+        private async Task<string> GetExpectedHtml()
+        {
+            using(var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Tranquire.Selenium.Tests.Html.html"))
+            {
+                var sr = new StreamReader(stream);
+                return await sr.ReadToEndAsync();
+            }
+        }
+    }
+}
