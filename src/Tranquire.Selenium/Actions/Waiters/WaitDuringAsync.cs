c08e5ef5cb844c74bdf972fc00626da1fe40538c
Lucas Loegel <galad@outlook.com>
2018-09-02 18:45:21 +0200

    Add Wait.During (#83)
    * Add Wait.During
    
    * Fix test
    
    * Fix test

---
+++ src/Tranquire.Selenium/Actions/Waiters/WaitDuringAsync.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Tranquire.Selenium.Actions.Waiters
+{
+    /// <summary>
+    /// Wait during the specified amount of time
+    /// </summary>
+    public sealed class WaitDuringAsync : Action<Task>
+    {
+        private readonly TimeSpan _timeToWait;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="WaitDuringAsync"/>
+        /// </summary>
+        /// <param name="timeToWait">The time to wait</param>
+        public WaitDuringAsync(TimeSpan timeToWait)
+        {
+            _timeToWait = timeToWait;
+        }
+
+        /// <inheritdoc />
+        public override string Name => $"Wait during ${_timeToWait}";
+
+        /// <inheritdoc />
+        protected override async Task ExecuteWhen(IActor actor)
+        {
+            await Task.Delay(_timeToWait).ConfigureAwait(false);
+        }
+    }
+}
