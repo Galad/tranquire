1591193c2d1f1f7c9d36b66167732eb26f583cf3
Lucas Loegel <galad@outlook.com>
2018-12-16 12:35:55 +0100

    Generate steps for methods with more than one parameter

---
+++ src/Tranquire.SpecFlow.Generation/Generator/EnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tranquire.SpecFlow.Generation.Generator
+{
+    internal static class EnumerableExtensions
+    {
+        public static string Join(this IEnumerable<string> source, string separator)
+        {
+            return string.Join(separator, source);
+        }
+    }
+}
