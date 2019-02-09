ae80f3c18f4764d774c4b5b56e9dc4a345bb95ca
Lucas Loegel <galad@outlook.com>
2019-02-09 15:45:15 +0100

    ITarget.RelativeTo creates a new target using a CSS selector, when both targets can be composed as a CSS selector (#99)

--- src/Tranquire.Selenium/Targets/TargetBy.cs
+++ src/Tranquire.Selenium/Targets/TargetBy.cs
@@ -1,4 +1,7 @@
 ﻿using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace Tranquire.Selenium.Targets
 {
@@ -24,6 +27,86 @@ namespace Tranquire.Selenium.Targets
         protected override ISearchContext SearchContext(IWebDriver webDriver)
         {
             return webDriver;
+        }
+
+        /// <inheritdoc />
+        public override ITarget RelativeTo(ITarget targetSource)
+        {
+            if (!(targetSource is TargetBy sourceTargetBy))
+            {
+                return base.RelativeTo(targetSource);
+            }
+
+            var (sourceValue, sourceByType) = GetByInfo(sourceTargetBy.By);
+            var (relativeValue, relativeByType) = GetByInfo(By);
+
+            if (sourceByType == ByType.Other || relativeByType == ByType.Other)
+            {
+                return base.RelativeTo(targetSource);
+            }
+
+            var by = By.CssSelector(AsCssSelector(sourceValue, sourceByType) + " " + AsCssSelector(relativeValue, relativeByType));
+            return new TargetBy(by, Name);
+
+            string AsCssSelector(string value, ByType byType)
+            {
+                switch (byType)
+                {
+                    case ByType.Id:
+                        return "#" + value;
+                    case ByType.ClassName:
+                        return "." + value;
+                    case ByType.CssSelector:
+                        return value;
+                    case ByType.Name:
+                        return "[name=" + value + "]";
+                    case ByType.TagName:
+                        return value;
+                    case ByType.Other:
+                    default:
+                        return value;
+                }
+            }
+        }
+               
+        private enum ByType
+        {
+            Id,
+            ClassName,
+            CssSelector,
+            Name,
+            TagName,
+            Other
+        }
+
+        private static PropertyInfo _byDescriptionProperty
+            = typeof(By).GetProperty("Description", BindingFlags.NonPublic | BindingFlags.Instance);
+        private static Regex _byDescriptionRegex = new Regex(@"(?<type>By\.[^:]*):\s(?<value>.*)", RegexOptions.Compiled);
+        private static Dictionary<string, ByType> _byTypeMap = new Dictionary<string, ByType>()
+        {
+            { "By.Id", ByType.Id },
+            { "By.ClassName[Contains]", ByType.ClassName },
+            { "By.CssSelector", ByType.CssSelector },
+            { "By.Name", ByType.Name },
+            { "By.TagName", ByType.TagName }
+        };
+
+        private static (string selector, ByType type) GetByInfo(By by)
+        {
+            var description = (string)_byDescriptionProperty.GetValue(by);
+            var match = _byDescriptionRegex.Match(description);
+            if (!match.Success)
+            {
+                return (null, ByType.Other);
+            }
+
+            var type = match.Groups["type"].Value;
+            if (!_byTypeMap.TryGetValue(type, out var byType))
+            {
+                return (null, ByType.Other);
+            }
+            var value = match.Groups["value"].Value;
+            return (value, byType);
         }
     }
 }
