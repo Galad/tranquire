9bc6766862bd23cf087bdfae01dbbc288a1edef7
Lucas Loegel <galad@outlook.com>
2019-03-16 15:43:53 +0100

    Support initialization of a UI model with a constructor

---
+++ src/Tranquire.Selenium.Tests/Questions/AttributesTests.cs
@@ -0,0 +1,33 @@
+﻿using AutoFixture;
+using AutoFixture.Idioms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tranquire.Selenium.Questions.UIModels;
+using Xunit;
+
+namespace Tranquire.Selenium.Tests.Questions
+{
+    public class AttributesTests
+    {
+        public static IEnumerable<object[]> Attributes
+        {
+            get
+            {
+                return typeof(TargetAttribute)
+                    .Assembly
+                    .GetTypes()
+                    .Where(t => typeof(TargetAttribute).IsAssignableFrom(t) || typeof(UIStateAttribute).IsAssignableFrom(t))
+                    .Select(t => new object[] { t });
+            }
+        }
+
+        [Theory, MemberData(nameof(Attributes))]
+        public void VerifyAttributesGuardClauses(Type attributeType)
+        {
+            var fixture = new Fixture();
+            var assertion = new GuardClauseAssertion(fixture);
+            assertion.Verify(attributeType.GetConstructors());
+        }
+    }
+}
