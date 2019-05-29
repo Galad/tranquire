31e454a597d46ebb2a629cdd4edcead201052f1f
Lucas Loegel <galad@outlook.com>
2019-05-29 22:33:52 +0200

    Use SelectMany in demo application

--- src/ToDoList.Automation/Api/Remove.cs
+++ src/ToDoList.Automation/Api/Remove.cs
@@ -7,15 +7,6 @@ namespace ToDoList.Automation.Api
     public static class Remove
     {
         public static IAction<Task> ToDoItem(string title) => Get.TheToDoItem(title)
-                                                                 .SelectMany<Task<Model.ToDoItem>, Task>(itemTask =>
-                                                                 {
-                                                                     return Actions.Create(
-                                                                         $"Remove to-do item",
-                                                                         async actor =>
-                                                                         {
-                                                                             var item = await itemTask;
-                                                                             return actor.Execute(new RemoveToDoItem(item.Id));
-                                                                         });
-                                                                 });
+                                                                 .SelectMany(item => new RemoveToDoItem(item.Id));
     }
 }
