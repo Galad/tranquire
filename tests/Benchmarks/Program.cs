17ae03e53ba7997289daf0e7d0fe6f7aaeca464c
Lucas Loegel <galad@outlook.com>
2019-05-31 04:59:39 +0200

    Add benchmark project

---
+++ tests/Benchmarks/Program.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length > 1)
+            {
+                Console.WriteLine("Invalid number of arguments");
+                return;
+            }
+            var flag = args.Length == 0 ? "-r" : args[0];
+            
+            if(flag == "-r")
+            {
+                BenchmarkRunner.Run(typeof(Program).Assembly);
+            }
+            else if(flag == "-d")
+            {
+                BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+            }
+        }
+    }
+}
