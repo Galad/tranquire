e2c63e22ee201523d73f95c3a0a4605a793f3669
Lucas Loegel <galad@outlook.com>
2019-05-31 04:59:39 +0200

    Add benchmark project

---
+++ tests/Benchmarks/ActionsExecutionBenchmark.cs
@@ -0,0 +1,101 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+using System;
+using System.Linq;
+using Tranquire;
+
+namespace Benchmarks
+{
+    [MemoryDiagnoser]
+    [Q1Column, MedianColumn, Q3Column]
+    [EtwProfiler]
+    public class ActionsExecutionBenchmark
+    {
+        private readonly int[] _ints;
+        private readonly Actor _actor;
+        private readonly IAction<Unit> _sumUnit;
+        private readonly IAction<int> _sum;
+        private readonly SumClass _sumClass = new SumClass();
+
+        public ActionsExecutionBenchmark()
+        {
+            _ints = Enumerable.Range(0, 10).ToArray();
+            _actor = new Actor("John");
+            _sumUnit = SumUnit(_ints);
+            _sum = Sum(_ints);
+            _selectManySum = _ => _sum;
+        }
+
+        private static IAction<Unit> SumUnit(int[] values) => Actions.Create("sum", a => { values.Sum(); });
+        private static IAction<int> Sum(int[] values) => Actions.Create("sum", a => values.Sum());
+
+        [Benchmark]
+        public Unit Tranquire_OnTheFlyActions_Composite()
+        {
+            return _actor.Given(
+                new DefaultCompositeAction("actions",
+                SumUnit(_ints),
+                SumUnit(_ints),
+                SumUnit(_ints),
+                SumUnit(_ints)
+                ));
+        }
+
+        [Benchmark]
+        public int Tranquire_OnTheFlyActions_SelectMany()
+        {
+            IAction<int> action = Sum(_ints).SelectMany(_ => Sum(_ints))
+                                            .SelectMany(_ => Sum(_ints))
+                                            .SelectMany(_ => Sum(_ints));
+            return _actor.Given(action);
+        }
+
+        [Benchmark]
+        public Unit Tranquire_InstanciatedActions_Composite()
+        {
+            return _actor.Given(
+                   new DefaultCompositeAction("actions",
+                   _sumUnit,
+                   _sumUnit,
+                   _sumUnit,
+                   _sumUnit
+                   ));
+        }
+
+        [Benchmark]
+        public int Tranquire_InstanciatedActions_SelectMany()
+        {
+            IAction<int> action = _sum.SelectMany(_ => _sum)
+                                    .SelectMany(_ => _sum)
+                                    .SelectMany(_ => _sum);
+            return _actor.Given(action);
+        }
+
+        private Func<int, IAction<int>> _selectManySum;
+        [Benchmark]
+        public int Tranquire_InstanciatedFunc_SelectMany()
+        {
+            IAction<int> action = _sum.SelectMany(_ => _sum)
+                                    .SelectMany(_ => _sum)
+                                    .SelectMany(_ => _sum);
+            return _actor.Given(action);
+        }
+
+        private class SumClass
+        {
+            public int Sum(int[] values) => values.Sum();
+        }
+
+        [Benchmark]
+        public int Objects()
+        {
+            return _sumClass.Sum(_ints) + _sumClass.Sum(_ints) + _sumClass.Sum(_ints) + _sumClass.Sum(_ints);
+        }
+
+        [Benchmark(Baseline = true)]
+        public int Inline()
+        {
+            return _ints.Sum() + _ints.Sum() + _ints.Sum() + _ints.Sum();
+        }
+    }
+}
---
+++ tests/Benchmarks/DeepActionExecutionBenchmark.cs
@@ -0,0 +1,124 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+using System.Linq;
+using Tranquire;
+
+namespace Benchmarks
+{
+    [MemoryDiagnoser]
+    [Q1Column, MedianColumn, Q3Column]
+    [EtwProfiler]
+    public class DeepActionExecutionBenchmark
+    {
+        private readonly int[] _ints;
+        private readonly Actor _actor;
+        private readonly Actor _actorReporting;
+        private readonly DeepAction _deepAction;
+        private readonly SumClass _sumClass = new SumClass(1);
+
+        public DeepActionExecutionBenchmark()
+        {
+            _ints = Enumerable.Range(0, 10).ToArray();
+            _actor = new Actor("John");
+            _actorReporting = new Actor("Bob").WithReporting(new Tranquire.Reporting.XmlDocumentObserver());
+            _deepAction = new DeepAction(1, _ints);
+        }
+
+        private static IAction<Unit> SumUnit(int[] values) => Actions.Create("sum", a => { values.Sum(); });
+        private static IAction<int> Sum(int[] values) => Actions.Create("sum", a => values.Sum());
+        private const int MaxDepth = 10;
+
+        [Benchmark]
+        public int Tranquire_OnTheFlyActions()
+        {
+            return _actor.Given(new DeepActionOnTheFly(1, _ints));
+        }
+
+        private class DeepActionOnTheFly : ActionBase<int>
+        {
+            private readonly int _depth;
+            private readonly int[] _ints;
+
+            public DeepActionOnTheFly(int depth, int[] ints)
+            {
+                _depth = depth;
+                _ints = ints;
+            }
+
+            public override string Name => "action";
+
+            protected override int ExecuteWhen(IActor actor)
+            {
+                if (_depth == MaxDepth)
+                {
+                    return _ints.Sum();
+                }
+                return actor.Execute(new DeepActionOnTheFly(_depth + 1, _ints));
+            }
+        }
+
+        [Benchmark]
+        public int Tranquire_OnTheFlyActions_Instanciated()
+        {
+            return _actor.Given(_deepAction);
+        }
+
+        [Benchmark]
+        public int Tranquire_OnTheFlyActions_Instanciated_WithReporting()
+        {
+            return _actorReporting.Given(_deepAction);
+        }
+
+        private class DeepAction : ActionBase<int>
+        {
+            private readonly int _depth;
+            private readonly int[] _ints;
+            private readonly DeepAction _next;
+
+            public DeepAction(int depth, int[] ints)
+            {
+                _depth = depth;
+                _ints = ints;
+                _next = depth < MaxDepth ? new DeepAction(depth + 1, ints) : null;
+            }
+
+            public override string Name => "action";
+
+            protected override int ExecuteWhen(IActor actor)
+            {
+                if (_next is null)
+                {
+                    return _ints.Sum();
+                }
+                return actor.Execute(_next);
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public int Objects()
+        {
+            return _sumClass.Sum(_ints);
+        }
+
+        private class SumClass
+        {
+            private readonly int _depth;
+            private readonly SumClass _next;
+
+            public SumClass(int depth)
+            {
+                _depth = depth;
+                _next = depth < MaxDepth ? new SumClass(depth + 1) : null;
+            }
+
+            public int Sum(int[] values)
+            {
+                if (_next is null)
+                {
+                    return values.Sum();
+                }
+                return _next.Sum(values);
+            }
+        }
+    }
+}
