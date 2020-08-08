22abc2c3f54c0f2114940a98fc4d2d44449db2fc
Lucas Loegel <galad@outlook.com>
2020-08-08 12:50:22 +0200

    Add hit actions

--- src/Tranquire.Selenium/Actions/Hit.cs
+++ src/Tranquire.Selenium/Actions/Hit.cs
@@ -10,7 +10,10 @@ public class Hit : TargetableAction<EnterValue>
     /// <summary>
     /// Gets the keys to hit
     /// </summary>
-    public string Keys { get; }
+    public string HitKeys { get; }
+
+    /// <inheritdoc />
+    public string Name => $"Hit the keys {HitKeys}";
 
     /// <summary>
     /// Creates a new instance of <see cref="Hit"/>
@@ -18,7 +21,7 @@ public class Hit : TargetableAction<EnterValue>
     /// <param name="keys">The keys to hit</param>
     public Hit(string keys) : base(t => new EnterValue(keys, t))
     {
-        Keys = keys;
+        HitKeys = keys;
     }
 
     /// <summary>
@@ -28,5 +31,23 @@ public class Hit : TargetableAction<EnterValue>
     public static Hit Enter()
     {
         return new Hit(OpenQA.Selenium.Keys.Enter);
+    }
+
+    /// <summary>
+    /// Creates an action which hit the Enter key
+    /// </summary>
+    /// <returns></returns>
+    public static Hit Escape()
+    {
+        return new Hit(OpenQA.Selenium.Keys.Escape);
+    }
+
+    /// <summary>
+    /// Creates an action which hit the Enter key
+    /// </summary>
+    /// <returns></returns>
+    public static Hit Keys(string keys)
+    {
+        return new Hit(keys);
     }
 }
