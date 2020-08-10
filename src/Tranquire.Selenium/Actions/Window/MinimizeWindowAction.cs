c5288da1ced11f26b923b68984e8ef654283bfba
Lucas Loegel <galad@outlook.com>
2020-08-10 15:23:20 +0200

    Add window actions

---
+++ src/Tranquire.Selenium/Actions/Window/MinimizeWindowAction.cs
@@ -0,0 +1,21 @@
+﻿namespace Tranquire.Selenium.Actions.Window
+{
+    /// <summary>
+    /// Minimizes the window
+    /// </summary>
+    public class MinimizeWindowAction : ActionBaseUnit<WebBrowser>
+    {
+        /// <inheritdoc />
+        public override string Name => "Minimize Window";
+
+        /// <summary>
+        /// Execute the action
+        /// </summary>
+        /// <param name="actor"></param>
+        /// <param name="ability"></param>
+        protected override void ExecuteWhen(IActor actor, WebBrowser ability)
+        {
+            ability.Driver.Manage().Window.Minimize();
+        }
+    }
+}
